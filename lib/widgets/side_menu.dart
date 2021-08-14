bc6614f1d5f7d1fd918d10f3fa7af602ab4ce74a
Tom <Tomy51451@gmail.com>
2021-08-14 18:41:29 +0200

    :sparkles: NEW: Add side menu

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/data/data.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -23,7 +24,147 @@ class SideMenu extends StatelessWidget {
               ),
             ],
           ),
+          SideMenuIconTab(
+            iconData: Icons.home,
+            title: "Home",
+            onTap: () {},
+          ),
+          SideMenuIconTab(
+            iconData: Icons.search,
+            title: "Search",
+            onTap: () {},
+          ),
+          SideMenuIconTab(
+            iconData: Icons.audiotrack,
+            title: "Radio",
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          LibraryPlaylist()
         ],
+      ),
+    );
+  }
+}
+
+class SideMenuIconTab extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final VoidCallback onTap;
+
+  const SideMenuIconTab({
+    Key? key,
+    required this.iconData,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      leading: Icon(
+        iconData,
+        size: 28,
+        color: Theme.of(context).iconTheme.color,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText1,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+}
+
+class LibraryPlaylist extends StatefulWidget {
+  const LibraryPlaylist({Key? key}) : super(key: key);
+
+  @override
+  _LibraryPlaylistState createState() => _LibraryPlaylistState();
+}
+
+class _LibraryPlaylistState extends State<LibraryPlaylist> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Scrollbar(
+        isAlwaysShown: true,
+        controller: _scrollController,
+        child: ListView(
+          controller: _scrollController,
+          padding: EdgeInsets.symmetric(vertical: 12.0),
+          physics: const ClampingScrollPhysics(),
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    "YOUR LIBRARY",
+                    style: Theme.of(context).textTheme.bodyText1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                ...yourLibrary
+                    .map(
+                      (e) => ListTile(
+                        title: Text(
+                          e,
+                          style: Theme.of(context).textTheme.bodyText2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                    .toList()
+              ],
+            ),
+            const SizedBox(height: 24.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Text(
+                    "PLAYLISTS",
+                    style: Theme.of(context).textTheme.bodyText1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                ...playlists
+                    .map(
+                      (e) => ListTile(
+                        title: Text(
+                          e,
+                          style: Theme.of(context).textTheme.bodyText2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                    .toList()
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
